410d190af89e1a6334b1055afd15377535ae3d22
Yannis Karakozis <I-C-Karakozis@users.noreply.github.com>
2018-09-23 12:44:53 -0400

    info update

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -13,23 +13,23 @@ class Contact extends React.Component {
         />
         <div id="contact">
           <div className="row">
-            <div className="col-sm-12 col-md-12">
-              <h3>President and Founder</h3>
-              <p>Yannis Karakozis ick@princeton.edu</p>
+            <div className="col-sm-6 col-md-6">
+              <h3>President and League Administrator</h3>
+              <p>Sonny Huang '20, shengh@</p>
+            </div>
+            <div className="col-sm-6 col-md-6">
+              <h3>Founder and Head of Team Building</h3>
+              <p>Yannis Karakozis '19, ick@</p>
             </div>
           </div>
           <div className="row">
-            <div className="col-sm-4 col-md-4">
+            <div className="col-sm-6 col-md-6">
               <h3>Financial Coordinator</h3>
-              <p>Kayla Dobies</p>
+              <p>Kayla Dobies '20</p>
             </div>
-            <div className="col-sm-4 col-md-4">
-              <h3>League Administrator</h3>
-              <p>Sonny Huang</p>
-            </div>
-            <div className="col-sm-4 col-md-4">
+            <div className="col-sm-6 col-md-6">
               <h3>Director of Public Relations</h3>
-              <p>Autumn Weyant</p>
+              <p>Autumn Weyant '20</p>
             </div>
           </div>
           <div>
